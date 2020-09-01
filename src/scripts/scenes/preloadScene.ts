5cc049ff4ad077f144ba33e1f6d755732b89c5d4
Katarina <katarina@udel.edu>
2020-08-31 21:25:23 -0400

    mass calculated

--- src/scripts/scenes/preloadScene.ts
+++ src/scripts/scenes/preloadScene.ts
@@ -10,6 +10,10 @@ export default class PreloadScene extends Phaser.Scene {
     this.load.image("blackBox", "assets/images/blackBox.png");
     this.load.image("balance", "assets/images/balance.png");
     this.load.image("evapDish", "assets/images/evapDish.png");
+    this.load.image("jar", "assets/images/jar.png");
+    this.load.image("addButton", "assets/images/addButton.png");
+    this.load.image("removeButton", "assets/images/removeButton.png");
+    this.load.image("resetButton", "assets/images/resetButton.png");
   }
 
   create() {
--- src/scripts/scenes/weighScene.ts
+++ src/scripts/scenes/weighScene.ts
@@ -1,5 +1,6 @@
 import { GameObjects } from "phaser";
 import moveableThing from "../objects/moveableThing";
+import button from "../objects/button";
 
 export default class weighScene extends Phaser.Scene{
 
@@ -13,6 +14,9 @@ export default class weighScene extends Phaser.Scene{
     private evapDish: moveableThing;
     private hitBox: GameObjects.Image;
     private totalMass: number;
+    private jar: GameObjects.Image;
+    private addButton: button;
+    private resetButton: button;
 
 
     constructor(){
@@ -23,14 +27,16 @@ export default class weighScene extends Phaser.Scene{
         this.pickHydrateNumber();
         this.findEvapDishMass();
         this.blackBox=this.add.image(400, 350, "blackBox");
-        this.blackBox.setTintFill(0x323236);
-        
+        this.blackBox.setTintFill(0x2a2a2e);
+        this.hitBox=this.physics.add.image(200, 230, "blackBox");
+        this.hitBox.setScale(0.3);
 
         this.balance=this.add.image(200, 225, "balance");
         this.balance.setScale(0.80);
-        this.hitBox=this.physics.add.image(200, 230, "blackBox");
-        this.hitBox.setScale(0.3);
+        this.jar=this.add.image(700, 250, "jar");
+        this.jar.setScale(0.5);
         
+        this.additionNumber=0;
         this.mass = 0.000001;
         this.massLabel=this.add.bitmapText(220, 300, "calibriFont", this.mass.toFixed(2)+" g");
         this.massLabel.setFontSize(40);
@@ -39,6 +45,13 @@ export default class weighScene extends Phaser.Scene{
         this.evapDish.setScale(0.6);
 
         this.physics.add.overlap(this.evapDish, this.hitBox, this.updateMassLabel, undefined, this);
+    
+    
+        this.addButton = new button(this, 700, 40, "addButton", 0.7);
+        this.addButton.on('pointerdown', ()=>this.changeAdditions("add"), this);
+    
+        this.resetButton = new button(this, 45, 385, "resetButton", 0.7);
+        this.resetButton.on('pointerdown', ()=>this.reset(), this);
     }
 
     pickHydrateNumber(){
@@ -54,5 +67,24 @@ export default class weighScene extends Phaser.Scene{
     updateMassLabel(){
         this.totalMass = this.mass + this.massEvapDish;
         this.massLabel.text=this.totalMass.toFixed(2) + " g";
+    }
+
+    changeAdditions(name: string){
+        if (name == "add"&&this.additionNumber<5){
+            this.additionNumber++;
+        }
+        this.changeCompoundMass();
+    }
+
+    changeCompoundMass(){
+        this.mass=1.5*this.additionNumber + Math.round(Math.random()*100)/100;
+        console.log("compound mass: " + this.mass.toFixed(2));
+    }
+
+    reset(){
+        this.massLabel.text=0.0001.toFixed(2) + " g";
+
+        this.evapDish.x=520;
+        this.evapDish.y=300;
     }
 }
